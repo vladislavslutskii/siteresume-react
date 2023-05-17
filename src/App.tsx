562bfd1c9eddea76c4a188b94b3aa9b662869376
Vladislav <102241947+vladislavslutskii@users.noreply.github.com>
2023-05-17 19:45:15 +0300

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./app.module.scss";
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
